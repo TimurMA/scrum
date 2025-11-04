feb5c0b4db357edc352dfd3317a2c0873f0f75fd
TimurMA <timur.minyazeff@gmail.com>
2025-11-04 22:49:41 +0500

    sprint data generation

--- backend/src/main/java/com/tyuiu/backend/models/entities/Sprint.java
+++ backend/src/main/java/com/tyuiu/backend/models/entities/Sprint.java
@@ -20,7 +20,7 @@ public class Sprint {
     private String name;
     private String goal;
     private String report;
-    private LocalDateTime startDate;
+    private LocalDateTime startDate = LocalDateTime.now();
     private LocalDateTime finishDate;
     private SprintStatus status;
 

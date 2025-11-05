25d27e022a42cd1d0727cc95dbb3392b79e73d0c
TimurMA <timur.minyazeff@gmail.com>
2025-11-05 09:08:03 +0500

    add rsocket client to frontend

---
+++ frontend/src/composables/useRSocket.ts
@@ -0,0 +1,68 @@
+import { Ref, ref } from "vue";
+import {
+  RSocketClient,
+  JsonSerializer,
+  IdentitySerializer,
+} from "rsocket-core";
+import TcpClientTransport from "rsocket-tcp-client";
+
+async function useRSocket<Type>(subscribeUrl: string, socketData?: Type) {
+  const data = ref<Type>() as Ref<Type>;
+  const isConnected = ref(false);
+
+  const client = new RSocketClient({
+    serializers: {
+      data: JsonSerializer,
+      metadata: IdentitySerializer,
+    },
+    setup: {
+      keepAlive: 60000,
+      lifetime: 180000,
+      dataMimeType: "application/json",
+      metadataMimeType: "message/x.rsocket.routing.v0",
+    },
+    transport: new TcpClientTransport({
+      host: "localhost",
+      port: 8080,
+    }),
+  });
+
+  function closeConnection() {
+    client.close();
+  }
+
+  await new Promise((resolve, reject) => {
+    client.connect().then(
+      (socket) => {
+        resolve((isConnected.value = true));
+
+        socket
+          .requestStream({
+            metadata: String.fromCharCode(subscribeUrl.length) + subscribeUrl,
+          })
+          .subscribe({
+            onError: (error) => reject(error),
+            onNext: (payload) => {
+              if (socketData instanceof Array) {
+                socketData.push(payload.data);
+              } else {
+                socketData = payload.data;
+              }
+
+              if (data.value instanceof Array) {
+                data.value.push(payload.data);
+              } else {
+                data.value = payload.data;
+              }
+            },
+            onSubscribe: (subscirbe) => subscirbe.request(1000),
+          });
+      },
+      (error) => reject(error)
+    );
+  });
+
+  return { data, isConnected, closeConnection };
+}
+
+export default useRSocket;
